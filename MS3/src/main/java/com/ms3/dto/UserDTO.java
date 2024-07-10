41c30f4bd4f3796731d96f6c3594039d570ce1b2
TEEPUT <ciyi1594@gmail.com>
2024-07-10 14:08:16 +0900

    🔴 마이크로 서비스 추가

---
+++ MS3/src/main/java/com/ms3/dto/UserDTO.java
@@ -0,0 +1,99 @@
+package com.ms3.dto;
+
+import org.apache.ibatis.type.Alias;
+
+@Alias("user")
+public class UserDTO {
+	private String id;
+	private int grantNo;
+	private String password;
+	private String email;
+	private String name;
+	private String nickname;
+	private String profile;
+	
+	public UserDTO() {
+	}
+
+	public UserDTO(String id, int grantNo, String password, String email, String name, String nickname,
+			String profile) {
+		super();
+		this.id = id;
+		this.grantNo = grantNo;
+		this.password = password;
+		this.email = email;
+		this.name = name;
+		this.nickname = nickname;
+		this.profile = profile;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public int getGrantNo() {
+		return grantNo;
+	}
+
+	public void setGrantNo(int grantNo) {
+		this.grantNo = grantNo;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getProfile() {
+		return profile;
+	}
+
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+
+	@Override
+	public String toString() {
+		return "UserDTO [id=" + id + ", grantNo=" + grantNo + ", password=" + password + ", email=" + email + ", name="
+				+ name + ", nickname=" + nickname + ", profile=" + profile + "]";
+	}
+	
+	
+
+	
+	
+	
+	
+	
+}
