3788bd50b7c5aa995d330fed878d225875ccdaed
주로 <ju8027@naver.com>
2024-07-11 16:47:46 +0900

    push

---
+++ MS3/src/main/java/com/ms3/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.ms3.mapper;
+
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.ms3.dto.UserDTO;
+
+@Mapper
+public interface UserMapper {
+    int insertUser(UserDTO dto);
+    UserDTO selectUser(Map<String, Object> map);
+}
