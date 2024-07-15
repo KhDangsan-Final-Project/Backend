ab3d4208403b98299dd0f0294ec86cf12dc98e02
주로 <ju8027@naver.com>
2024-07-15 15:02:18 +0900

    관리자 페이지

---
+++ MS4/src/main/java/com/ms4/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.ms4.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/")
+	public String index () {
+		return "user_list";
+	}
+}
